8dc1795fd0edfba8c43eb6a9b4c84b7ad2dcb2c2
Aryan <aryankansal15@gmail.com>
2025-10-24 20:49:14 +0530

    news icon updated

--- routes/news.routes.js
+++ routes/news.routes.js
@@ -4,5 +4,8 @@ const router = Router();
 router.get("/", (req,res)=>{
     res.render("news.ejs");
 })
+router.get("/temp", (req,res)=>{
+    res.render("newsTemp.ejs");
+})
 
 export default router
